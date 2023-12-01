7015d51d538c830031313a60c012cb653c0c4fba
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-01 17:02:15 +0200

    first commit

---
+++ app/src/main/java/com/example/recyclepro/auth/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.recyclepro.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recyclepro.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
