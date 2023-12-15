a90240abf7b1053ce711eb0f50196b6b01c151bb
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-15 19:52:21 +0200

    finish desgin

---
+++ app/src/main/java/com/example/recyclepro/home/UpdateProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.recyclepro.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.recyclepro.R;
+
+public class UpdateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+    }
+}
